063fc0d66b8024e9268c8d14618d32a5ab687c00
Fsain228 <michail30178@gmail.com>
2025-04-16 09:18:36 +0300

    Bug Fix

--- FSLog/__init__.py
+++ FSLog/__init__.py
@@ -1,10 +1,35 @@
-__version__ = "0.1.0" 
+__version__ = "0.1.2"
 
 from typing import Tuple
+from .fslog import FSLog, Colors, create_logger
 
-from .fslog import (FSLog, Colors, create_logger)
+_default_logger = FSLog()
 
-__all__: Tuple[str, ...] = ("FSLog", "Colors", "create_logger",)
+__all__: Tuple[str, ...] = (
+    "Colors",
+    "create_logger",
+    "server",
+    "logging",
+    "error",
+    "warning",
+    "info",
+    "debug",
+    "custom",
+    "log",
+    "lib_debug",
+    "logging_stats",
+)
+
+server = _default_logger.server
+logging = _default_logger.logging
+error = _default_logger.error
+warning = _default_logger.warning
+info = _default_logger.info
+debug = _default_logger.debug
+custom = _default_logger.custom
+log = _default_logger.log
+lib_debug = _default_logger.lib_debug
+logging_stats = _default_logger.logging_stats
 
 def __dir__() -> Tuple[str, ...]:
     return __all__ + ("__version__", "__doc__")
